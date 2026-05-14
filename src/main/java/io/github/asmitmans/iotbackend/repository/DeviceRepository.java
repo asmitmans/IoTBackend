8944385831252faa1369115f8ee615a81df31580
asmitmans <a.smitmans@gmail.com>
2026-05-13 23:40:47 -0400

    feat: add query methods to DeviceRepository

--- src/main/java/io/github/asmitmans/iotbackend/repository/DeviceRepository.java
+++ src/main/java/io/github/asmitmans/iotbackend/repository/DeviceRepository.java
@@ -1,4 +1,13 @@
 package io.github.asmitmans.iotbackend.repository;
 
-public interface DeviceRepository {
+import io.github.asmitmans.iotbackend.entity.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Optional<Device> findByApiKeyPrefix(String apiKeyPrefix);
+    Optional<Device> findBySerialNumber(String serialNumber);
 }
