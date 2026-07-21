a218962c664a2d62154e3ae96617125d48401688
asmitmans <a.smitmans@gmail.com>
2026-07-20 23:44:22 -0400

    fix: make UserDetailsServiceImpl.loadUserByUsername transactional (LazyInitializationException on Account.publicId)

--- src/main/java/io/github/asmitmans/iotbackend/security/UserDetailsServiceImpl.java
+++ src/main/java/io/github/asmitmans/iotbackend/security/UserDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
@@ -19,6 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                              .map(user -> new UserPrincipal(
